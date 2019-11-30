de74849bdf2311bbe1c6894790835022f9b71533
tobiasalanboyd <eboyd@udel.edu>
2019-11-30 14:12:03 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {} from 'googlemaps';
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-gmaps';
+
+  @ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
+
+  ngOnInit(): void {
+    const mapProperties = {
+         center: new google.maps.LatLng(35.2271, -80.8431),
+         zoom: 15,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+ }
 }
